eaccf33d3abbd1408572e1e2df31a9a51c56b221
Paolo Martinez <paolomartinezucsd@gmail.com>
2022-05-13 13:19:06 -0700

    Completed GitHub Finder project

--- github-finder/app.js
+++ github-finder/app.js
@@ -1,5 +1,7 @@
 // Init GitHub
 const github = new Github();
+// Init UI
+const ui = new UI();
 
 // Search input
 const searchUser = document.getElementById('searchUser');
@@ -15,14 +17,15 @@ searchUser.addEventListener('keyup', (e) => {
       .then(data => {
         if(data.profile.message === 'Not Found') {
           // Show alert
-          
+          ui.showAlert('User not found', 'alert alert-danger');
         } else {
           // Show profile
-
+          ui.showProfile(data.profile);
+          ui.showRepos(data.repos);
         }
       })
   } else {
     // Clear profile
-
+    ui.clearProfile();
   }
 });
--- github-finder/github.js
+++ github-finder/github.js
@@ -1,16 +1,22 @@
 class Github {
   constructor() {
     this.client_id = '84512449d0504cb2b762';
-    this.client_secret = '4a11d3281b0ee1ba3796d253799b839427fc0519';
+    this.client_secret = '33ea61da484bfc361ecfb745a1d726fda5007b1e';
+    this.repos_count = 5;
+    this.repos_sort = 'created: asc';
   }
 
+  //TODO update the authorization to use access token; currently we are still limited in number of requests we can make
   async getUser(user) {
     const profileResponse = await fetch(`https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`);
+    const repoResponse = await fetch(`https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}&client_id=${this.client_id}&client_secret=${this.client_secret}`);
 
     const profile = await profileResponse.json();
+    const repos = await repoResponse.json();
 
     return {
-      profile
+      profile,
+      repos
     }
   }
 }
